7ba788479971394399e06d5b9a950397b3585571
CincoSwim <cgpucko@gmail.com>
2020-08-27 18:52:07 -0400

    Initial commit

---
+++ hw0b.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+bool chkPrime (int num, int * point){
+    if (num == 1){
+        return true;
+    }
+    if (num!=2 ){
+        if (num < 2 ) {
+            *point = 1;
+            return false;
+        }
+        for (int i = 2; i <= sqrt(num); i++)
+            if (num % i == 0) {
+                *point = i;
+                return false;
+            }
+    }
+    return true;
+}
+
+
+int main() {
+    int numInput, divisibleBy;
+    int * pointer;
+    bool isPrime;
+    pointer = &divisibleBy;
+
+    cout << "Please Enter an Integer for test:";
+    cin >> numInput;
+    cout<< endl << "You entered: " << numInput << endl;
+    isPrime = chkPrime(numInput, pointer);
+    if (isPrime)
+        cout<< "This number is Prime!";
+    if (!isPrime)
+        cout << "This number is not prime, and is divisible by " << *pointer;
+
+
+
+
+
+
+
+    return 0;
+}
